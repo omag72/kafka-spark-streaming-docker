46828fe97bfb9f13c9a3d8c6f84dc42773b6305a
Owen <44193948+omag72@users.noreply.github.com>
2023-08-17 16:14:12 +0200

    Update producer.py

--- python-producer/producer.py
+++ python-producer/producer.py
@@ -1,12 +1,46 @@
 from kafka import KafkaProducer
+import json
 import time
+import random
 
-producer = KafkaProducer(bootstrap_servers="kafka:9092")
+# Kafka broker address
+KAFKA_BROKER = "localhost:9092"
 
-while True:
+# Kafka topic to produce to
+TOPIC = "test-topic"
 
-    message = b"Hello From Python " + bytes(str(time.time()), "utf-8")
+def generate_data():
+    data = {
+        "timestamp": int(time.time()),
+        "value": round(random.uniform(0, 100), 2),
+        "status": random.choice(["normal", "warning", "error"]),
+    }
+    return json.dumps(data)
 
-    producer.send("test-topic", key=b"python-message", value=message)
-    print(message)
-    time.sleep(2)
+if __name__ == "__main__":
+    # Create a Kafka producer instance
+    producer = KafkaProducer(
+        bootstrap_servers=KAFKA_BROKER,
+        value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    )
+
+    try:
+        while True:
+            # Generate data
+            message = generate_data()
+
+            # Produce the message to the specified topic
+            producer.send(TOPIC, value=message)
+
+            print("Message sent:", message)
+            time.sleep(2)  # Simulate generating data every 2 seconds
+
+    except KeyboardInterrupt:
+        pass
+
+    # Close the producer
+    producer.close()
+
+
+
+    
