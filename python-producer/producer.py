c5c3374cdca69e41ad80da5920ad637aa6463d18
Owen <44193948+omag72@users.noreply.github.com>
2023-08-17 12:46:42 +0200

    Update producer.py

--- python-producer/producer.py
+++ python-producer/producer.py
@@ -1,12 +1,45 @@
-from kafka import KafkaProducer
+import json
 import time
+import random
+from faker import Faker
+from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers="kafka:9092")
+KAFKA_BROKER = "kafka:9092"
+TOPIC = "test-topic"
 
-while True:
+fake = Faker()
 
-    message = b"Hello From Python " + bytes(str(time.time()), "utf-8")
+def generate_weather_data():
+    weather_data = {
+        "timestamp": int(time.time()),
+        "location": fake.city(),
+        "temperature": round(random.uniform(-10, 40), 2),
+        "humidity": round(random.uniform(10, 90), 2),
+        "weather_condition": fake.random_element(elements=("Sunny", "Cloudy", "Rainy", "Snowy")),
+    }
+    return json.dumps(weather_data)
 
-    producer.send("test-topic", key=b"python-message", value=message)
-    print(message)
-    time.sleep(2)
+def delivery_report(err, msg):
+    if err is not None:
+        print("Message delivery failed: {}".format(err))
+    else:
+        print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
+
+if __name__ == "__main__":
+    producer_config = {
+        "bootstrap.servers": KAFKA_BROKER,
+    }
+
+    producer = KafkaProducer(producer_config)
+
+    try:
+        while True:
+            weather_data = generate_weather_data()
+            print(weather_data)
+            producer.produce(TOPIC, weather_data.encode("utf-8"), callback=delivery_report)
+            producer.poll(0.5)  # Poll for events, adjust interval as needed
+            time.sleep(2)  # Simulate generating data every 2 seconds
+    except KeyboardInterrupt:
+        pass
+
+    producer.flush()
